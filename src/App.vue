9d34f28952b4fcc062c27c864e2f0d5e550e3a85
Vilberg Eliasson <vilbergs@gmail.com>
2021-03-25 19:01:24 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+
+  <BarChart />
+
+</template>
+
+<script setup>
+import BarChart from './components/BarChart.vue'
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
