e65add7fbbe78494923baa9544b2c4bb1a8ee7bb
AndreaOsk <andrea.eliasd@gmail.com>
2021-04-12 11:30:15 +0100

    Card.vue component imported into App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
   <BarChart />
-
+  <Card />
 </template>
 
 <script setup>
-import BarChart from './components/BarChart.vue'
-
+import BarChart from "./components/BarChart.vue";
+import Card from "./components/Card.vue";
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Lato", Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  display: flex;
+  justify-content: space-evenly;
 }
 </style>
